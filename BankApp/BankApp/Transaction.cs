9d50aa2efd7309f741991b89779cd9301766bacf
Adam Gonzales <adgonz@microsoft.com>
2018-10-25 09:02:52 -0700

    10-25-2018 V1.3

---
+++ BankApp/BankApp/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace BankApp
+{
+    enum TransactionType
+    {
+        Credit,
+        Debit
+    }
+    class Transaction
+    {
+        public int TransactionID { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public string Descrition { get; set; }
+        public TransactionType TypeOfTransaction { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
