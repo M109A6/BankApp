537339eb964f4b9db75bdedc89b18edd48be99e0
Adam Gonzales <adam@gonz.cloud>
2018-11-01 09:12:05 -0700

    11/1/2018 rev 1
    11/1/2018 rev 1

---
+++ BankApp/BankApp/Migrations/20181101145525_Transactions.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BankApp.Migrations
+{
+    public partial class Transactions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transaction_Accounts_AccountNumber",
+                table: "Transaction");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Transaction",
+                table: "Transaction");
+
+            migrationBuilder.RenameTable(
+                name: "Transaction",
+                newName: "Transactions");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Transaction_AccountNumber",
+                table: "Transactions",
+                newName: "IX_Transactions_AccountNumber");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Transactions",
+                table: "Transactions",
+                column: "TransactionID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Accounts_AccountNumber",
+                table: "Transactions",
+                column: "AccountNumber",
+                principalTable: "Accounts",
+                principalColumn: "AccountNumber",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Accounts_AccountNumber",
+                table: "Transactions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Transactions",
+                table: "Transactions");
+
+            migrationBuilder.RenameTable(
+                name: "Transactions",
+                newName: "Transaction");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Transactions_AccountNumber",
+                table: "Transaction",
+                newName: "IX_Transaction_AccountNumber");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Transaction",
+                table: "Transaction",
+                column: "TransactionID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transaction_Accounts_AccountNumber",
+                table: "Transaction",
+                column: "AccountNumber",
+                principalTable: "Accounts",
+                principalColumn: "AccountNumber",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
